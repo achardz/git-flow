2e7c43abf41a9da900f79d04f8d3d16cec372cfc
Dev <i3igrock@gmail.com>
2026-07-08 12:32:33 +0700

    feat: add divide() [WIP, not yet merged]

--- src/calculator.js
+++ src/calculator.js
@@ -19,4 +19,9 @@ function multiply(a, b) {
   return a * b;
 }
 
-module.exports = { add, subtract, multiply };
+function divide(a, b) {
+  if (b === 0) throw new Error('divide by zero');
+  return a / b;
+}
+
+module.exports = { add, subtract, multiply, divide };
