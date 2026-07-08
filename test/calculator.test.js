8e756c4046e6d424bb72c5d7a4af3159c36ef0da
Dev <i3igrock@gmail.com>
2026-07-08 12:31:07 +0700

    feat: add multiply()

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -5,5 +5,6 @@ const calc = require('../src/calculator');
 
 assert.strictEqual(calc.add(2, 3), 5, 'add');
 assert.strictEqual(calc.subtract(5, 2), 3, 'subtract');
+assert.strictEqual(calc.multiply(4, 3), 12, 'multiply');
 
 console.log('All tests passed');
